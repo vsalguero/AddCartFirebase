a54ddf22ebe8c08b253d8ccfcc35828befb30b21
Vladimir Salguero <vsalgueroerazo@gmail.com>
2023-04-16 21:22:34 -0600

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+
+addButtonEl.addEventListener("click", function(){
+    console.log("IT works!");
+})
