3fdc682bad54cf7179ed86d7c2e1f0a0ea906abb
Vladimir Salguero <vsalgueroerazo@gmail.com>
2023-04-16 22:23:15 -0600

    chore: create input styles

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
 
 addButtonEl.addEventListener("click", function(){
-    console.log("IT works!");
+    let inputValue = inputFieldEl.value;
+    
 })
