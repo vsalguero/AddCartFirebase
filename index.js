f30fa2ae370c0695d47527f5a541aeda9d1fb030
Vladimir Salguero <vsalgueroerazo@gmail.com>
2023-04-16 23:04:30 -0600

    chore: firebase database connection

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { projectId, databaseUrl } from "../config.js";
+
+const appSettings = {
+    projectId,
+    databaseUrl
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
 
-addButtonEl.addEventListener("click", function(){
+addButtonEl.addEventListener("click", function () {
     let inputValue = inputFieldEl.value;
-    
-})
+    push(shoppingListInDB, inputValue);
+});
