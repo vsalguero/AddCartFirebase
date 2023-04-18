4e830f0c6466fea3b56d8819696412785d9fe353
Vladimir Salguero <vsalgueroerazo@gmail.com>
2023-04-17 22:55:31 -0600

    chore: fetch data from firebase

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 import { projectId, databaseUrl } from "../config.js";
 
 const appSettings = {
@@ -18,14 +18,26 @@ const shoppingListEl = document.getElementById("shopping-list");
 addButtonEl.addEventListener("click", function () {
     let inputValue = inputFieldEl.value;
     push(shoppingListInDB, inputValue);
-
-    appendItemToShoppingListEl(inputValue);
-
     clearInputFieldEl();
+});
+
+onValue(shoppingListInDB, function (snapshot) {
+    let itemsArray = Object.values(snapshot.val());
+
+    cleanShoppingListEl();
+
+    for (let i = 0; i < itemsArray.length; i++) {
+        let currentValue = itemsArray[i];
+        appendItemToShoppingListEl(currentValue);
+    }
 });
 
 function clearInputFieldEl() {
     inputFieldEl.value = "";
+}
+
+function cleanShoppingListEl() {
+    shoppingListEl.innerHTML = "";
 }
 
 function appendItemToShoppingListEl(itemValue) {
